c7b8003ec550ee2d9c0228fcd7a6ca657d14a682
Jay Engineer <jayengineer6@gmail.com>
2026-06-17 20:14:12 -0400

    added microsoft analytics

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
+import Clarity from "@microsoft/clarity";
 import { routeTree } from "./routeTree.gen";
+
+if (typeof window !== "undefined") {
+  Clarity.init("x8p1rlrqwv");
+}
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
